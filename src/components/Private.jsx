62e80c473a24f911b933f46b83e4f3bded5d0c74
Pankaj Pareek <71495471+pankajpareek026@users.noreply.github.com>
2022-12-22 19:47:34 +0530

    Delete src directory

--- src/components/Private.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Outlet,Navigate } from 'react-router-dom'
-export default function Private()
-{const Isuser=localStorage.getItem('user')
-    return  !Isuser?<Outlet/>:<Navigate to={'/'}/>
-}
--- src/components/Protected.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-
-export default function Protected() {
-    const Isuser = localStorage.getItem('user')
-    return Isuser ? <Outlet /> : <Navigate to={'/login'} />;
-}
