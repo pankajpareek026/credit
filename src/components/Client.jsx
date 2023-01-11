a29f5ff6b100734d3ed5c296045fb01a91a8a6c7
Pankaj Pareek <71495471+pankajpareek026@users.noreply.github.com>
2023-01-11 16:05:16 +0530

    UI update

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 const Client = ({name,amount,Id}) => {
   return (
-
-    <div className="client">
-    <span className="name"><Link style={{color:"white",textDecoration:"none"}} to ={`/detail/${Id}`} >{name}</Link></span>
-    <span className="date">12-09-2022</span>
-    <span className="amount"> {amount}</span>
-</div>
+<>
+<tr>
+  <td>
+  <Link style={{color:"white",textDecoration:"none"}} to ={`/detail/${Id}`} >{name}</Link>
+</td>
+<td>12-09-2022</td>
+<td >{amount}</td></tr>
+</>
   )
 }
 
 
-export default Client
+export default Client;
