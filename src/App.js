d3472903801384525d1bcb73ae30316615509a3a
Pankaj Pareek <71495471+pankajpareek026@users.noreply.github.com>
2023-06-22 22:44:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,23 @@ import Private from './components/Private';
 import Detail from './components/Detail';
 
 function App() {
+  document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
   return (
     <div className="app">
 
