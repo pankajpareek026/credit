f7bd4bfb0da9113278ccc52db4169f052531e2f0
pankaj pareek <pankajpareek70168@gmail.com>
2023-08-20 00:49:16 +0530

    ...

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 
 export default function Detail({amount,date,dis,type}) {
-    document.title = "d"
-<<<<<<< HEAD
-   
-=======
->>>>>>> 8865ec46904dbe8a703f54c24a6eb619692c120e
+
+
 
     return (
         <div className="details">
