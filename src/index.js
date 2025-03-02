2fc8bcf5d1d38f3b58e839744d5db82fa48e972f
Pankaj Pareek <71495471+pankajpareek026@users.noreply.github.com>
2025-03-02 16:58:05 +0530

    Update index.js to Enable PWA

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Import service worker registration
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <Provider store={store}>
     <App />
   </Provider>
 );
+
+//  service worker registration  to enable PWA features
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
